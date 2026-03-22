71c5d316521a5f2ffaf0da04a8b2001528bb3c3d
lily <jroc050988@gmail.com>
2026-03-22 13:00:37 +0800

    新增路徑常數

---
+++ src/router/route-constant.ts
@@ -0,0 +1,10 @@
+export const PATH = {
+  home: '/',
+  user: '/user',
+  admin:'/admin',
+
+  offline: '/admin/offline',
+  offlineOrder: '/admin/offline/order',
+
+  online: '/admin/online',
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import AppGreetings from '../components/AppGreetings.vue'
+import { PATH } from "@/router/route-constant";
+import AppGreetings from "../components/AppGreetings.vue";
 </script>
 
 <template>
@@ -10,8 +11,8 @@ import AppGreetings from '../components/AppGreetings.vue'
       <AppGreetings name="ㄌㄌ代購" />
 
       <nav>
-        <RouterLink to="/user">使用者前台系統</RouterLink>
-        <RouterLink to="/admin">管理者後台系統</RouterLink>
+        <RouterLink :to="PATH.user">使用者前台系統</RouterLink>
+        <RouterLink :to="PATH.admin">管理者後台系統</RouterLink>
       </nav>
     </div>
   </div>
@@ -21,24 +22,23 @@ import AppGreetings from '../components/AppGreetings.vue'
 </template>
 
 <style>
-   .home{
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-self: center;
-    .wrapper{
-      margin-left: 2rem;
-    }
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-self: center;
+  .wrapper {
+    margin-left: 2rem;
   }
-  nav {
-    margin-left: auto;
-    margin-top: 1rem;
-    display: flex;
-    gap: 1rem;
-    a {
-      border: 1px solid #00bd7e;
-      padding: 0.25rem 0.75rem;
-    }
+}
+nav {
+  margin-left: auto;
+  margin-top: 1rem;
+  display: flex;
+  gap: 1rem;
+  a {
+    border: 1px solid #00bd7e;
+    padding: 0.25rem 0.75rem;
   }
+}
 </style>
-
