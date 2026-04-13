5042eb3f2988f313a0f6cb4e8b434c58a3ae8141
lily <jroc050988@gmail.com>
2026-04-13 17:12:49 +0800

    調整包貨管理api

--- src/services/api/customer-orders/customer-orders-api.ts
+++ src/services/api/customer-orders/customer-orders-api.ts
@@ -8,7 +8,7 @@ import type {
   GetChannelBonusRes,
 } from './customer-orders-api-interfaces'
 
-/** 個人購物清單相關 API 集合 */
+/** 包貨管理與客戶訂單相關 API 集合 */
 export const customerOrdersApi = {
   /**
    * 查詢活動內有訂單的客戶清單
@@ -16,16 +16,15 @@ export const customerOrdersApi = {
    * @returns 有訂單的客戶清單
    */
   getCustomers: async (req: GetCustomerOrdersCustomersReq): Promise<GetCustomerOrdersCustomersRes> => {
-    return await getApi('/stats/customer-orders/customers', req)
+    return await getApi('/packing/customers', req)
   },
-
   /**
    * 查詢客戶在活動內的個人訂單清單
    * @param req - 包含客戶 ID 與活動 ID 的查詢條件
    * @returns 該客戶的訂單清單
    */
   getCustomerOrders: async (req: GetCustomerOrdersReq): Promise<GetCustomerOrdersRes> => {
-    return await getApi('/stats/customer-orders', req)
+    return await getApi('/packing/orders', req)
   },
 
   /**
@@ -34,6 +33,6 @@ export const customerOrdersApi = {
    * @returns 各通路滿額狀態清單
    */
   getChannelBonus: async (req: GetChannelBonusReq): Promise<GetChannelBonusRes> => {
-    return await getApi('/stats/customer-orders/channel-bonus', req)
+    return await getApi('/packing/channel-bonus', req)
   },
 }
